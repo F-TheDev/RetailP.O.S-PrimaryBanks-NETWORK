9c811b8188e0f3a273665b193e3587fbfb00e627
f_tube <schazadkhesreavi2000@gmail.com>
2026-05-15 13:37:51 +0200

    TEST_1

--- [ARCHIVED ] NETWORK.js
+++ [ARCHIVED ] NETWORK.js
@@ -1,6 +1,6 @@
 {
-  "maintenance": false,
-  "latestVersion": 1,
-  "minSupportedVersion": 1,
+  "maintenance": true,
+  "latestVersion": 2,
+  "minSupportedVersion": 2,
   "message": "WARNING | This system is outdated and is no longer in use please download the LATEST VERSION | FTS ©"
 }
